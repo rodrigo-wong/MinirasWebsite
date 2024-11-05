788659529621a6ef8676d2f9578a5bdf6dba4cc7
rodrigo-wong <wongmacr@gmail.com>
2024-11-04 20:13:33 -0500

    Refactor App.js to use a reusable Section component for each section of the page

--- src/App.js
+++ src/App.js
@@ -6,27 +6,45 @@ import About from "./components/About";
 import Summary from "./components/Summary";
 import Services from "./components/Services";
 import ContactForm from "./components/ContactForm";
+import { useInView } from "react-intersection-observer";
+
+const Section = ({ children, id, className }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3, // Adjust the threshold as needed
+  });
+
+  return (
+    <div
+      id={id}
+      ref={ref}
+      className={`${className} section ${inView ? "fade-in" : "fade-out"}`}
+    >
+      {children}
+    </div>
+  );
+};
 
 function App() {
   return (
     <>
       <Navbar className="sticky-top" />
       <div className="bg-light">
-        <div id="home" className="full-height-lg section">
+        <Section id="home" className="full-height-lg">
           <Home />
-        </div>
-        <div id="about" className="section">
+        </Section>
+        <Section id="about">
           <About />
-        </div>
-        <div id="summary" className="section" style={{ marginTop: "10em" }}>
+        </Section>
+        <Section id="summary" style={{ marginTop: "10em" }}>
           <Summary />
-        </div>
-        <div id="services" className="section" style={{ marginTop: "10em" }}>
+        </Section>
+        <Section id="services" style={{ marginTop: "10em" }}>
           <Services />
-        </div>
-        <div id="contact" className="section" style={{ marginTop: "10em" }}>
+        </Section>
+        <Section id="contact" style={{ marginTop: "10em" }}>
           <ContactForm />
-        </div>
+        </Section>
       </div>
     </>
   );

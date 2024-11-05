a5a14b71b81f32164842a0bfb424d3d9ef46085d
rodrigo-wong <wongmacr@gmail.com>
2024-11-04 19:30:54 -0500

    Refactor Services component to add responsive font sizes for different screen sizes and include a Maps component with Leaflet integration

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import Maps from "./Maps";
 
 const Services = () => {
   return (
     <Container className="my-5">
       <h2 className="text-center mb-5">Services</h2>
-      <Row className="justify-content-center fs-5 m-3 m-md-4 m-lg-5 g-3">
-        
+      <Row className="justify-content-center responsive-text m-3 m-md-4 m-lg-5 g-3">
         {/* First Column: Lesson Types */}
-        <Col
-          xs={12}
-          md={7}
-          lg={8}
-          className="mb-4"
-        >
+        <Col xs={12} md={7} lg={8} className="mb-4">
           <Card className="shadow border-0 h-100 rounded-3 p-3">
             <Card.Body>
               <Card.Title className="text-center fw-bold mb-3">
@@ -53,7 +48,28 @@ const Services = () => {
             </Card.Body>
           </Card>
         </Col>
-        
+
+        <Col xs={12} md={5} lg={4} className="mb-4">
+          <Card className="shadow border-0 h-100 rounded-3 p-3">
+            <Card.Body>
+              <Card.Title className="text-center fw-bold mb-3">
+                Location
+              </Card.Title>
+              <Card.Text>
+                <div>
+                  <p>
+                    I offer in-person lessons in <strong>Burlington, ON, Canada</strong>. My
+                    studio is conveniently located near the intersection of 
+                    <strong> Walkers Line and Upper Middle Road</strong>.
+                  </p>
+                </div>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} md={7} lg={8} className="mb-4">
+          <Maps />
+        </Col>
       </Row>
     </Container>
   );

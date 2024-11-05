9471a9bf74dc3c5a491d6b4cfb4cd45af21bfcbc
rodrigo-wong <wongmacr@gmail.com>
2024-11-04 19:30:40 -0500

    Refactor ContactForm component to add responsive font sizes for different screen sizes and include an image for credentials

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Container,
+  Row,
+  Col,
+  Alert,
+  Image,
+} from "react-bootstrap";
 import emailjs from "emailjs-com";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
@@ -43,7 +51,7 @@ const ContactForm = () => {
       {submitted && (
         <Alert
           variant="success"
-          className="text-center fs-5 mx-3 mx-md-4 mx-lg-5 g-3"
+          className="text-center responsive-text mx-3 mx-md-4 mx-lg-5 g-3"
         >
           Thank you for your message!
         </Alert>
@@ -51,17 +59,20 @@ const ContactForm = () => {
       {error && (
         <Alert
           variant="danger"
-          className="text-center fs-5 mx-3 mx-md-4 mx-lg-5 g-3"
+          className="text-center responsive-text mx-3 mx-md-4 mx-lg-5 g-3"
         >
           Oops! Something went wrong. Please try again.
         </Alert>
       )}
-      <Row className="d-flex align-items-stretch fs-5 mx-3 mx-md-4 mx-lg-5 g-3">
-        <Col md={12} lg={12} className="d-flex">
+      <Row className="d-flex align-items-stretch responsive-text mx-3 mx-md-4 mx-lg-5 g-3">
+        <Col md={8} lg={8} className="d-flex">
           <Form
             onSubmit={handleSubmit}
             className="p-4 shadow contact-form w-100"
           >
+            <Container className="text-center mb-4">
+              <h4>Get in touch</h4>
+            </Container>
             <Form.Group controlId="formName" className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
@@ -105,6 +116,13 @@ const ContactForm = () => {
               </Button>
             </div>
           </Form>
+        </Col>
+        <Col md={4} lg={4} className="d-none d-lg-block">
+          <Image
+            src="/assets/IMG_5907.jpg"
+            className="w-100 rounded-3 shadow"
+            alt="Credentials"
+          />
         </Col>
       </Row>
     </Container>
